0a740e90b1e401d302eb5692efe1b0c919891e94
Anton Norell <anton.norell@evidente.se>
2019-11-01 15:49:42 +0100

    Add uploadActivity
    Work in progress

--- StravaApi/StravaApi.py
+++ StravaApi/StravaApi.py
@@ -6,7 +6,7 @@ import yaml
 class StravaApi:
 
     def __init__(self):
-        self.baseUrl = 'https://www.strava.com/api/v3/'
+        self.baseUrl = 'https://www.strava.com/api/v3'
         self.clientId = 0
         self.clientSecret = ''
         self.readConfig()
@@ -46,14 +46,15 @@ class StravaApi:
                                            , headers=tokenParams)
         return oauthToken
 
-    def post(self, url, payload):
-        return requests.post(url, headers=self.headers, data=payload)
+    def post(self, url, payload, files=None):
+        return requests.post(url, headers=self.headers, data=payload, files=files)
 
     def get(self, url):
         return requests.get(url, headers=self.headers)
 
 
     def createActivity(self, name, type, startDate, elapsedTime, distance, description="", trainer=0, commute=0):
+        url = self.baseUrl + '/activities'
         payload = {
                    'name' : name
                  , 'type' : type
@@ -65,19 +66,37 @@ class StravaApi:
                  , 'photo_ids' : ''
                  , 'commute' : commute}
 
-        url = baseUrl + 'activities'
-
-        return post(url, payload)
+        return self.post(url, payload)
 
 
     def getAthlete(self ):
-        return get(self.baseUrl + 'athlete')
+        url = self.baseUrl + '/athlete'
+        return self.get(url)
+
+    def uploadActivity(self, file, name, description, trainer, commute, data_type, externalId):
+        url = self.baseUrl + "/uploads"
+        payload = {
+                      'file@': file
+                    , 'name' : name
+                    , 'description' : description
+                    , 'trainer' : trainer
+                    , 'commute' : commute
+                    , 'data_type' : data_type
+                    , 'external_id' : externalId
+                  }
+
+        files = {'upload_file': file}
+        return self.post(url, payload, files)
+
 
 
 def main():
     api = StravaApi()
     currentTime = datetime.datetime.now().isoformat()
-    result = api.createActivity("Script Test", "Run", currentTime, 150, 5000, "Testing out the Strava API")
+   # result = api.createActivity("Script Test", "Run", currentTime, 150, 5000, "Testing out the Strava API")
+    filePath = "c:/Users/antonn/source/repos/StravaApiRepo/2014-03-10-173741.gpx"
+    uploadRes = api.uploadActivity(open(filePath, 'rb'), "2014-03-10-173741", "First test of uploading gpx file", "0", "0", "gpx", "0")
+    a = "a"
 
 
 if __name__ == "__main__":
